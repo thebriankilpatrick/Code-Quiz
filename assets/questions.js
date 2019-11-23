1bdad708c9a75bd63e21a4d221bb3171188195a5
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2019-11-23 00:07:05 -0500

    Added initial code, and added some questions to questions.js

---
+++ assets/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        title: "What is the name of Jon Snow's direwolf?",
+        choices: ["Grey Wind", "Nymeria", "Ghost", "Summer"],
+        answer: "Ghost"
+    },
+    {
+        title: "Who is Robert Baratheon referring to when he tells Ned Stark, 'In my dreams, I kill him every night'",
+        choices: ["Rhaegar Targaryen", "Stannis Baratheon", "Joffrey Baratheon", "Jaime Lannister"],
+        answer: "Rhaegar Targaryen"
+    },
+    {
+        title: "Who chopped off Jaime Lannister's hand?",
+        choices: ["Roose Bolton", "Locke", "jon Snow", "Brienne of Tarth"],
+        answer: "Locke"
+    },
+]

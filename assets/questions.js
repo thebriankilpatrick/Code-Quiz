38fe946fd1bc9ef54d49e0bb19e994cc17a0595e
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2019-11-26 18:33:18 -0500

    Added pseudocode, for steps to completion

--- assets/questions.js
+++ assets/questions.js
@@ -57,3 +57,37 @@ var questionSet = [
 var pageTitle = document.querySelector("title");
 // Grabbing my question container and setting it as a variable
 var questionContainer = document.querySelector(".questionContainer");
+// Grabbing my image and setting it as a variable
+var homeScreenImage = document.querySelector("#homeScreenImage");
+
+
+// Start game by clicking button, begin countdown timer
+// Page should change style, and begin quiz.  This should also start a timer
+// An answer will chime a sound, and display correct or incorrect
+// Then move onto next question
+// Points should be calculated based on how fast the question is answered (correctly)
+// Incorrect answers will result in a time penalty
+// Running out of time should end the game
+// At game's end, it should display a form to type your name
+// It will store your name in localStorage
+// The other button should populate a table, with all results displayed from form
+
+
+// Onclick event on button.  When clicked, run function.
+// Function will change setAttribute of body, to look more appropriate for a quiz
+// It should also start a countdown timer
+// At the end of timer, the first question should display.
+// Display questions by a function using setAttribute and textContent or innerHTML
+// Use eventListeners or onClick events on answers?  Or even if else?
+// If answer is incorrect, run function
+// Function will set timer to timer -= 10 (seconds), then display next question 
+// If answer is correct, run function
+// Function will calculate a score, then display next question.  Store points in a var
+// At the end of game, run function to display a form.
+// In this form, the var where the score is stored will display
+// The user can enter name, store is the localStorage
+
+
+function playGame() {
+    
+}
